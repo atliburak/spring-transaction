0e26818e982dab9160bbf707253085f7c0fb18ce
Burak ATLI <atliburak93@gmail.com>
2022-03-21 01:29:25 +0300

    first commit dev

--- src/main/java/com/example/springtransaction/SpringTransactionApplication.java
+++ src/main/java/com/example/springtransaction/SpringTransactionApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringTransactionApplication {
 
     public static void main(String[] args) {
+        System.out.println("asdasdasdasdasdsa");
         SpringApplication.run(SpringTransactionApplication.class, args);
     }
 
